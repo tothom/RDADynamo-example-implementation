eaedc8798401b6d02223ac13b5d1dba0a1516331
tothom <thomas.holth@gmail.com>
2023-07-25 11:31:47 +0200

    Update AssemblyInfo.cs

--- src/ExampleImplementation/Properties/AssemblyInfo.cs
+++ src/ExampleImplementation/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("zExampleImplementation")]
+[assembly: AssemblyTitle("ExampleImplementation")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("zExampleImplementation")]
+[assembly: AssemblyProduct("ExampleImplementation")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
